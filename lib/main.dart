e3752f3a9c727b4266a841c138fdf8f933c0127f
Agrim Gupta <agrim121@gmail.com>
2020-05-20 20:03:32 +0530

    UI Designed

--- lib/main.dart
+++ lib/main.dart
@@ -16,78 +16,148 @@ class _MyAppState extends State<MyApp> {
 
   final _nameController = TextEditingController();
   final _passwordController = TextEditingController();
+  bool _rememberMeFlag = false;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.lightGreen[400],
-          title: Text('Login Page'),
-        ),
+        resizeToAvoidBottomPadding: false,
         body: Container(
-          child: Card(
-            margin: EdgeInsets.all(1),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: <Widget>[
-                Card(
-                  elevation: 10,
-                  margin: EdgeInsets.all(25),
-                  child: TextFormField(
-                    controller: _nameController,
-                    decoration: const InputDecoration(
-                      icon: Icon(Icons.person),
-                      hintText: 'Required',
-                      labelText: 'Name *',
+          margin: EdgeInsets.all(20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'Complaint Corner',
+                style: TextStyle(
+                  fontSize: 40,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.blue[700],
+                ),
+              ),
+              Text(
+                'Powered by Vdoers',
+                style: TextStyle(
+                  color: Colors.blue[800],
+                  fontWeight: FontWeight.bold,
+                  fontSize: 15,
+                ),
+              ),
+              SizedBox(
+                width: 350,
+                height: 70,
+              ),
+              TextFormField(
+                maxLength: 10,
+                keyboardType: TextInputType.number,
+                controller: _nameController,
+                decoration: const InputDecoration(
+                  prefixIcon: Icon(Icons.phone_android),
+                  hintText: 'Required',
+                  labelText: 'Mobile Number ',
+                  counterText: '0/10',
+                  border: const OutlineInputBorder(),
+                  contentPadding:
+                      EdgeInsets.symmetric(vertical: 8, horizontal: 8),
+                  isDense: true,
+                ),
+              ),
+              SizedBox(
+                width: 150,
+                height: 20,
+              ),
+              TextFormField(
+                maxLength: 30,
+                controller: _passwordController,
+                obscureText: true,
+                decoration: const InputDecoration(
+                  counterText: '0/30',
+                  prefixIcon: Icon(Icons.lock),
+                  suffixIcon: Icon(
+                    Icons.visibility_off,
+                  ),
+                  border: const OutlineInputBorder(),
+                  hintText: 'Required',
+                  labelText: 'Password ',
+                  contentPadding:
+                      EdgeInsets.symmetric(vertical: 8, horizontal: 8),
+                  isDense: true,
+                ),
+              ),
+              // SizedBox(
+              //   height: 20,
+              // ),
+              Row(
+                children: <Widget>[
+                  GestureDetector(
+                    child: Checkbox(
+                      value: _rememberMeFlag,
+                      onChanged: (value) {
+                        setState(() {
+                          _rememberMeFlag = !_rememberMeFlag;
+                        });
+                      },
+                    ),
+                    onTap: () => setState(() {
+                      _rememberMeFlag = !_rememberMeFlag;
+                    }),
+                  ),
+                  Text(
+                    'Remember me',
+                    style: new TextStyle(color: Colors.black),
+                  ),
+                ],
+              ),
+              ButtonTheme(
+                minWidth: 310,
+                height: 50,
+                child: RaisedButton(
+                  color: Colors.blue[450],
+                  onPressed: () {
+                    setState(
+                      () {
+                        if (checkMandatoryFields()) {
+                          _name = _nameController.text;
+                          _password = _passwordController.text;
+                          //Call Login Api
+
+                          Fluttertoast.showToast(msg: 'Login Successful');
+                        }
+                      },
+                    );
+                  },
+                  child: Text(
+                    'Login',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold,
                     ),
                   ),
                 ),
-                Card(
-                  elevation: 10,
-                  margin: EdgeInsets.all(25),
-                  child: TextFormField(
-                    controller: _passwordController,
-                    obscureText: true,
-                    decoration: const InputDecoration(
-                      icon: Icon(Icons.lock),
-                      suffixIcon: Icon(
-                        Icons.visibility_off,
-                      ),
-                      hintText: 'Required',
-                      labelText: 'Password *',
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                ButtonTheme(
-                  minWidth: 310,
-                  height: 50,
-                  child: RaisedButton(
-                    color: Colors.lightGreen,
-                    onPressed: () {
-                      setState(() {
-                        _name = _nameController.text;
-                        _password = _passwordController.text;
-                      });
-                    },
-                    child: Text(
-                      'Login',
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-                ),
-                Text('Your name is $_name and Your password is $_password')
-              ],
-            ),
+              ),
+              SizedBox(height: 10,),
+              Text(
+                "Forgot password ?",
+                style: new TextStyle(color: Colors.pink[200],fontWeight: FontWeight.bold),
+              ),
+            ],
           ),
         ),
       ),
     );
+  }
+
+  bool checkMandatoryFields() {
+    if (_nameController.text.isEmpty) {
+      Fluttertoast.showToast(msg: 'UserName can not be empty');
+      return false;
+    } else if (_passwordController.text.isEmpty) {
+      Fluttertoast.showToast(msg: 'Password can not be empty');
+      return false;
+    } else {
+      return true;
+    }
   }
 }
